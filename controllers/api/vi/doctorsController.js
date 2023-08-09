6609aab3ce7f87851a3683545298a44ad5891678
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-09 09:30:40 +0530

    Doctors Controller completed

--- controllers/api/vi/doctorsController.js
+++ controllers/api/vi/doctorsController.js
@@ -1,5 +1,66 @@
+const Doctor = require("../../../models/doctor");
+
 module.exports.register = async (req, res) => {
-  return res.json({
-    message: "Doctors Controllers",
-  });
+  try {
+    let doctor = await Doctor.create(req.body);
+
+    return res.status(200).json({
+      success: true,
+      body: doctor,
+      message: "Doctor Registeration Successful",
+    });
+  } catch (err) {
+    console.log("Error in register Doctor details", err);
+    return res.status(400).json({
+      success: false,
+      message: "Error Occured in Registering Doctor details",
+    });
+  }
+};
+
+// Doctor login Controller
+module.exports.login = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Enter email or Password!",
+      });
+    }
+
+    let doctor = await Doctor.findOne({ email: email });
+
+    if (!doctor) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid username or Password!",
+      });
+    }
+
+    // Checking Password of username
+    const isMatch = await doctor.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid username or Password!",
+      });
+    }
+
+    const token = doctor.getSignedJwtToken();
+
+    return res.status(200).json({
+      success: true,
+      token,
+      message: `Log In Sucessful! Keep the Token safe ${doctor.name}!`,
+    });
+  } catch (err) {
+    console.log("Error in login Doctor account", err);
+    return res.status(400).json({
+      success: false,
+      message: "Error Occured in logging doctor account",
+    });
+  }
 };
--- routes/api/v1/doctors.js
+++ routes/api/v1/doctors.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const doctorController = require("../../../controllers/api/vi/doctorsController");
 
-router.get("/register", doctorController.register);
+router.post("/register", doctorController.register);
+router.post("/login", doctorController.login);
 
 module.exports = router;
