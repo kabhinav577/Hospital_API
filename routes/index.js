0d062df8b7be541cb0b9a41503ceaea1caa7b4ee
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-11 09:52:56 +0530

    Created README file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/", (req, res) => {
-  res.send(`<h1>Check Hospital or Covid-19 API Using POSTMAN</h1>`);
-});
 // Handling all API routes
 router.use("/api", require("./api"));
 
