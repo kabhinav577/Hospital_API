afeec9f55d227684f3f0cae40371624da541bdfd
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-11 10:47:47 +0530

    Few Changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send(`<h1>Testing Hospital or Covid-19 API Use POSTMAN</h1>`);
+});
 // Handling all API routes
 router.use("/api", require("./api"));
 
